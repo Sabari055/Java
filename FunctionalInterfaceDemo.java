0d8fed9f3a9d463c3fe45ad80021919a823cae4b
Sabari055 <sabari21032002@gmail.com>
2023-10-21 11:01:39 +0530

    FunctionalInterface

--- FunctionalInterfaceDemo.java
+++ FunctionalInterfaceDemo.java
@@ -3,9 +3,19 @@ interface demointerface {
     void show();
 }
 
+@FunctionalInterface
+interface demoReturn 
+{
+    int add(int n1, int n2);
+}
+
 public class FunctionalInterfaceDemo {
     public static void main(String[] args) {
         demointerface obj = () -> System.out.println("in show");  // this is lambda expression
         obj.show();
+
+        demoReturn obj1= (n1,n2)-> n1+n2; //lambda Expression for Return Type.
+        int res=obj1.add(3, 5);
+        System.out.println(res);
     }
 }
